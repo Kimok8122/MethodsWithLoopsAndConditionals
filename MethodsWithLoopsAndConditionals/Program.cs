16ef03e20659471c39ef09c944b0ed5ca510d659
Mona Lisa <email@example.com>
2020-08-27 11:09:25 -0600

    testing

--- MethodsWithLoopsAndConditionals/Program.cs
+++ MethodsWithLoopsAndConditionals/Program.cs
@@ -8,22 +8,26 @@ namespace MethodsWithLoopsAndConditionals
         {
             // Write a method that will print to the console all numbers 1000 through -1000.
 
-            var starting1000 = 1001;
+            public static void Print1000Range()
+            { 
 
-            do
-            {
-                starting1000 = --starting1000;
-              
-                Console.WriteLine("Decrease by 1:");
-                Console.WriteLine(starting1000);
+                var starting1000 = 1001;
 
-            } while (starting1000 >= -999);
+                do
+                {
+                    starting1000 = --starting1000;
 
-            Console.WriteLine("");
+                    Console.WriteLine("Decrease by 1:");
+                    Console.WriteLine(starting1000);
+
+                } while (starting1000 >= -999);
+
+                Console.WriteLine("");
+            }
 
             // Write a method that will print to the console numbers 3 through 999 by 3 each time.
 
-            var starting3 = 3;
+        var starting3 = 3;
 
             do
             {
